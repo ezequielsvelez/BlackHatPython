43a306caffb5446b37038a4187804405a2f87dab
ezequielsvelez <e.s.velez@gmail.com>
2021-01-31 16:46:57 -0800

    Chapter 2

---
+++ Chapter2/sum.py
@@ -0,0 +1,7 @@
+def sum(number_one, number_two):
+    number_one_int = int(number_one)
+    number_two_int = int(number_two)
+    return number_one_int+number_two_int
+
+answer = sum("1", "2")
+print(answer)
